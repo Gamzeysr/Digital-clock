0f222742216231cd770d459328c92eb344c7752d
Gamze <gamzeyasar35@icloud.com>
2022-10-03 09:22:07 +0300

    first commit

---
+++ dijital.js
@@ -0,0 +1,16 @@
+function Dijital_saat() {
+    let sure = new Date;
+    document.getElementById('saat').innerHTML = say(sure.getHours()) + ":"
+        + say(sure.getMinutes()) + ":"
+        + say(sure.getSeconds());
+    setInterval(Dijital_saat, 1000);
+}
+
+function say(x) {
+    if (x < 10) {
+        x = "0" + x;
+    }
+    return x;
+}
+
+Dijital_saat();
